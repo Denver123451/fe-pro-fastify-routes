12a6e7da1dc316d0c48dfb166b6d1a2a75e95ede
Denver12345 <villaan12345>
2022-08-18 15:20:16 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -15,28 +15,26 @@ fastify.post('/uppercase', (request, reply) => {
   const toUppCase = request.body.toUpperCase();
   if (toUppCase.includes('FUCK')) {
     return reply.status(403).send('unresolved');
-  };
+  }
   return reply.send(toUppCase);
 });
-
 
 fastify.post('/lowercase', (request, reply) => {
   const toLowCase = request.body.toLowerCase();
   if (toLowCase.includes('fuck')) {
     return reply.status(403).send('unresolved');
-  };
+  }
   return reply.send(toLowCase);
 });
 
 fastify.get('/user/:id', (request, reply) => {
-const {id} = request.params;
+  const { id } = request.params;
 
-if(users[id]) {
-  return reply.send(users[id]);
-}
-reply.status(400).send('User not exist');
-})
-
+  if (users[id]) {
+    return reply.send(users[id]);
+  }
+  reply.status(400).send('User not exist');
+});
 
 fastify.get('/users', (request, reply) => {
   // const queryFilter = request.query.filter;
@@ -47,18 +45,16 @@ fastify.get('/users', (request, reply) => {
   // })
   // reply.send(usersArray);
 
-  const {filter, value} = request.query
+  const { filter, value } = request.query;
   const usersArr = Object.values(users);
   if (!filter || !value) {
     return reply.send(usersArr);
-  }
-  else {
-    const usersArray = Object.values(users).filter(user => {
-        return user[filter].toString() === value;
-  });
+  } else {
+    const usersArray = usersArr.filter((user) => {
+      return user[filter].toString() === value;
+    });
     reply.send(usersArray);
-}
+  }
 });
-
 
 export default fastify;
